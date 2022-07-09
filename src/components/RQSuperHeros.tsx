1dc78b0a9e6c5cd414f353c97792ab9422d9c5c1
Kushal Raut <kushal.raut@dghub.io>
2022-07-09 22:44:48 +0545

    Exploring Additional configuration on get requests

--- src/components/RQSuperHeros.tsx
+++ src/components/RQSuperHeros.tsx
@@ -1,36 +1,59 @@
-import { useQuery } from 'react-query';
-import axios from 'axios';
+import { useQuery } from 'react-query'
+import axios from 'axios'
+import { useState } from 'react'
 
 const fetchSuperHeros = () => {
-  return axios.get('http://localhost:4000/superheros');
-};
+  return axios.get('http://localhost:4000/superheros')
+}
 
 const RQSuperHeros = () => {
-  const { data, isLoading, error, isFetching } = useQuery(
+  const [fetchInterval, setFetchInterval] = useState<any>(3000)
+
+  const onSuccess = (data: any) => {
+    console.log('Sideeffect of successful response')
+    if (data.data.length > 3) {
+      setFetchInterval(false)
+    }
+  }
+  const onError = (error: any) => {
+    console.log('Sideeffect of failed response', error)
+    setFetchInterval(false)
+  }
+
+  const { data, isLoading, error, isFetching, refetch } = useQuery(
     'super-heros',
     fetchSuperHeros,
     {
-      cacheTime: 5000,
+      // enabled: false,
+      onSuccess,
+      onError,
+      // cacheTime: 50000,
+      // staleTime: 30000,
+      // refetchOnMount: true,
+      // refetchOnWindowFocus: 'always',
+      refetchInterval: fetchInterval,
+      // refetchIntervalInBackground: true,
     }
-  );
+  )
 
-  if (isLoading) {
-    return <h2>Is Loading</h2>;
+  if (isLoading || isFetching) {
+    return <h2>Is Loading</h2>
   }
 
   if (error) {
-    return <h2>{(error as any).message}</h2>;
+    return <h2>{(error as any).message}</h2>
   }
-  console.log({ isLoading, isFetching });
+  console.log({ isLoading, isFetching })
 
   return (
     <>
       <h2>RQ Super Heros Page</h2>
+      {/* <button onClick={() => refetch()}>Fetch Data</button> */}
       {data?.data.map((hero: any) => {
-        return <div key={hero.name}>{hero.name}</div>;
+        return <div key={hero.name}>{hero.name}</div>
       })}
     </>
-  );
-};
+  )
+}
 
-export default RQSuperHeros;
+export default RQSuperHeros
